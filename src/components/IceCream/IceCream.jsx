329218a65b1cc98e30905813424b8fc8c3143bf9
Sayem Zaman <fzsayemofficial@gmail.com>
2022-07-21 17:02:14 +0600

    working

--- src/components/IceCream/IceCream.jsx
+++ src/components/IceCream/IceCream.jsx
@@ -6,12 +6,6 @@ const IceCream = () => {
     <div className={classes.ice_cream}>
       <div className={classes.cone}></div>
       <div className={`${classes.scoop} ${classes.lemon}`}></div>
-      <div className={`${classes.scoop} ${classes.vanilla}`}></div>
-      <div className={`${classes.scoop} ${classes.orange}`}></div>
-      <div className={`${classes.scoop} ${classes.chocolate}`}></div>
-      <div className={`${classes.scoop} ${classes.strawberry}`}></div>
-      <div className={`${classes.scoop} ${classes.lemon}`}></div>
-      <div className={`${classes.scoop} ${classes.orange}`}></div>
       <div className={classes.cherry}></div>
     </div>
   );
--- src/containers/IceCreamBuilder.jsx
+++ src/containers/IceCreamBuilder.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import Builder from '../components/Builder/Builder'
-import IceCream from '../components/IceCream/IceCream'
+import React, { Component } from "react";
+import Builder from "../components/Builder/Builder";
+import IceCream from "../components/IceCream/IceCream";
 
-const IceCreamBuilder = () => {
-  return (
-    <div class="container grid">
-      <IceCream/>
-      <Builder/>
-    </div>
-  )
+export default class IceCreamBuilder extends Component {
+  state = {
+    items: {
+      vanilla: 45,
+      chocolate: 58,
+      lemom: 35,
+      orange: 40,
+      strawberry: 68,
+    },
+    scoops: [],
+    totalPrice: 0,
+  };
+
+  render() {
+    const { items } = this.state;
+    return (
+      <div class="container grid">
+        <IceCream items={items} />
+        <Builder />
+      </div>
+    );
+  }
 }
-
-export default IceCreamBuilder
